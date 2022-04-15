ac29b9b34d6a757b40c6d36a882926f5a91ba837
Ray Huang <rayhuang@s-169-232-117-240.resnet.ucla.edu>
2022-04-15 00:40:48 -0700

    Deploy gh-pages

---
+++ out/_next/static/Uhgt8w_qhS4R5ebxJj4-U/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5801ea41cabe5d89.css","static/chunks/pages/index-b507ddac9e82ebf4.js"],"/_error":["static/chunks/pages/_error-350d9d86556f6d87.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
